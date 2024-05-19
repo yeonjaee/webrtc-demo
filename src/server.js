448c6de3c55aae97cb30d09163da9752bbfdfa7f
YeonJae Sung <siriyay@naver.com>
2024-05-19 16:47:54 +0900

    setup wss

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import ws from 'ws';
+import * as http from 'http';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-
 app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-app.get('/', (req, res) => res.render('home'));
+const server = http.createServer(app);
+const wss = new ws.WebSocketServer({ server });
 
-const handleListen = () => console.log('Server on http://localhost:3000');
-app.listen(3000, handleListen);
+wss.on('connection', (socket) => {
+  console.log('A new WebSocket connection has been established');
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'));
+  socket.on('message', (message) => {
+    console.log(message);
+  });
+  socket.send('hello!!!');
+});
+
+server.listen(3000, () => {
+  console.log('HTTP and WebSocket server running on http://localhost:3000');
+});
