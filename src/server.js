cbb7640c501553000863ac810eaa8292b3766308
YeonJae Sung <siriyay@naver.com>
2024-05-25 22:24:31 +0900

    use SocketIO

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import express from 'express';
 import ws from 'ws';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -10,28 +11,32 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
-const server = http.createServer(app);
-const wss = new ws.WebSocketServer({ server });
-const sockets = [];
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  console.log('A new WebSocket connection has been established');
-  socket.on('close', () => console.log('Disconnected from the Browser ❌'));
-  socket.on('message', (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case 'new_message':
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-        break;
-      case 'nickname':
-        socket['nickname'] = message.payload;
-    }
-  });
-});
 
-server.listen(3000, () => {
-  console.log('HTTP and WebSocket server running on http://localhost:3000');
+// const wss = new ws.WebSocketServer({ httpServer });
+// const sockets = [];
+//
+// /* const socket */
+// wss.on('connection', (socket) => {
+//   sockets.push(socket);
+//   console.log('A new WebSocket connection has been established');
+//   socket.on('close', () => console.log('Disconnected from the Browser ❌'));
+//   socket.on('message', (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case 'new_message':
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//         break;
+//       case 'nickname':
+//         socket['nickname'] = message.payload;
+//     }
+//   });
+// });
+
+httpServer.listen(3000, () => {
+  console.log('HTTP and WebSocket httpServer running on http://localhost:3000');
 });
